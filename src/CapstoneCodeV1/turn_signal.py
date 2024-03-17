64a2180f8bf4a68f8cbc30d1fe2def4971fd4cf8
junchuyan <91862909+junchuyan@users.noreply.github.com>
2024-03-17 15:39:51 -0700

    Add files via upload

---
+++ src/CapstoneCodeV1/turn_signal.py
@@ -0,0 +1,22 @@
+def turn_signal(trig, echo) 
+    turn = False
+
+    GPIO.output(trig, True)
+    time.sleep(0.00001)
+    GPIO.output(trig, False)
+
+    while GPIO.input(echo)==0:
+        start = time.time()
+
+    while GPIO.input(echo)==1:
+        end = time.time()
+
+    duration = end - start
+
+    distance = round(duration * 17150, 2)
+
+    if(distance < 10)
+        turn = True
+
+    return turn
+
