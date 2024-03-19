8b1e2094b2d94f14f913b92cc370f52d07d9cfae
Harry Jung <hj@Harrys-MacBook-Pro.local>
2024-03-18 23:31:24 -0700

    ultra sonnic v4

--- src/CapstoneCodeV1/HC_SR04_setup.py
+++ src/CapstoneCodeV1/HC_SR04_setup.py
@@ -46,7 +46,7 @@ if __name__ == '__main__':
         while True:
             dist = distance()
             print ("Measured Distance = %.1f cm" % dist)
-            time.sleep(1)
+            time.sleep(0.5)
  
         # Reset by pressing CTRL + C
     except KeyboardInterrupt:
