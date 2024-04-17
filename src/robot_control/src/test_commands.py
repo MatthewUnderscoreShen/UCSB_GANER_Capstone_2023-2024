26f1a26d03f7ac0373ca4ae23fb2f355e6192f19
Ubuntu <ubuntu@ubuntu.mshome.net>
2024-04-17 17:15:44 +0000

    joystick package added

--- src/robot_control/src/test_commands.py
+++ src/robot_control/src/test_commands.py
@@ -8,18 +8,17 @@ def test_callback(data):
 def main():
     rospy.init_node("test_drive_command")
 
+    test_condition_name = "placeholder" # link to file?
+
     sub = rospy.Subscriber("current_task", String, test_callback)    # topic name, msg type, callback method
-    pub = rospy.Publisher("drive_command", String, 10)  # topic name, msg type, queue size
+    pub = rospy.Publisher("drive_command", String, queue_size = 10)  # topic name, msg type, queue size
 
     while not rospy.is_shutdown():
         print("help")
-
-
-def testDriveCommand():
-    # Move forward a bit, then turn
-    print("soon")
-
-
+        # best way to get subscriber data?
+        sub_data = "placeholder"
+        if sub_data == test_condition_name:
+            pub.publish("test_command")
 
 if __name__ == '__main__':
     try:
