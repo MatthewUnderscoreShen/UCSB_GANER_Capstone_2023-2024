232e6a18254fcb37dfaebdea74ff101309605d7a
W <wang@WdeMacBook-Pro.local>
2024-04-23 22:38:05 -0700

    add movement

--- src/CapstoneCodeV1/Latch_Detect.py
+++ src/CapstoneCodeV1/Latch_Detect.py
@@ -62,9 +62,9 @@ if __name__ == '__main__':
             else:
                 print(xyxy)
                 if((xyxy[0][0]+xyxy[0][2])/2 > 340):
-                    movement(motor, 'left', 0.1)
-                if((xyxy[0][0]+xyxy[0][2])/2 < 300):
                     movement(motor, 'right', 0.1)
+                if((xyxy[0][0]+xyxy[0][2])/2 < 300):
+                    movement(motor, 'left', 0.1)
                 movement(motor,'stop',0.1)
                 xyxy = Detect_Object()
 
