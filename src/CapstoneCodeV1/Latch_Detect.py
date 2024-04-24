75fb0019e6fd8280f9072e3725517c4819d7f0e9
W <wang@WdeMacBook-Pro.local>
2024-04-23 21:52:11 -0700

    change

--- src/CapstoneCodeV1/Latch_Detect.py
+++ src/CapstoneCodeV1/Latch_Detect.py
@@ -3,12 +3,28 @@ from skimage import io
 import numpy as np
 import os
 
+import Control_Options_autonomous as ctl
+#import test_arm as ctl
+import xarm
+import math
+import RPi.GPIO as GPIO
+
 def Detect_Object():
     model = YOLO('best.pt') 
     os.system('raspistill -o image.jpg -h 640 -w 640 -t 10 -rot 0')
     results = model(['image.jpg'])  # return a list of Results objects
     xyxy = results[0].boxes.xyxy.numpy()
     print(xyxy)
+    return xyxy
 
 if __name__ == '__main__':
-    Detect_Object()
+    try:
+        xy = Detect_Object()
+        while True:
+            xy = Detect_Object()
+            
+
+    except KeyboardInterrupt:
+        print('Execution Aborted By User')
+        GPIO.cleanup()
+    
