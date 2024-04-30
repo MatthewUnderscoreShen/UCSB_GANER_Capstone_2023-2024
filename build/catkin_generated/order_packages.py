0da5a45d9024e4ea906605bbbb0dee98575fc0ab
MatthewUnderscoreShen <MatthewShen02@gmail.com>
2024-04-30 08:32:24 +0000

    ros update without the disaster

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/capstone/UCSB_GANER_Capstone_2023-2024/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/capstone/UCSB_GANER_Capstone_2023-2024/devel:$CMAKE_PREFIX_PATH"
+export PATH='/opt/ros/noetic/bin:/home/capstone/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PWD='/home/capstone/UCSB_GANER_Capstone_2023-2024/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/capstone/UCSB_GANER_Capstone_2023-2024/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/capstone/UCSB_GANER_Capstone_2023-2024/src:$ROS_PACKAGE_PATH"
