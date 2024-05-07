4eb32d4bc643d998e01bbbd42878b33ac9a7a16a
W <wang@WdeMacBook-Pro.local>
2024-05-06 20:08:13 -0700

    c

--- src/CapstoneCodeV1/Control_Options_Terminal.py
+++ src/CapstoneCodeV1/Control_Options_Terminal.py
@@ -124,12 +124,13 @@ def Terminal_Control(motor):
             L2 = 6.3
             L3 = 7
             parsed_elements = [float(element.strip()) for element in elements[1:]]
+            checkpoint = 1
             [Base,Arm_Extend,Elbow,Wrist] = IK(parsed_elements[0],parsed_elements[1],z = parsed_elements[2],L1=L1,L2=L2,L3=L3,gribber_angle=parsed_elements[3],currentBase = parsed_elements[4])
             
             if(Arm_Extend > np.pi/2):
                 print("too close")
                 Arm_Extend = np.pi/2
-            checkpoint = 1
+            
             arm.setPosition(3, round(500 - 700 * (Wrist/np.pi)), wait=False)
             arm.setPosition(4, round(450 - 600 * (Elbow/np.pi)), wait=False)
             arm.setPosition(5, round(800 - 600 * Arm_Extend/np.pi), wait=False)
