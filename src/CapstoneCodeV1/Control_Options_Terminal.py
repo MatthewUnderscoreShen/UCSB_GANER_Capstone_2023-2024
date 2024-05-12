1ffa1a1306970d8737ea2f702dfb7595e755c589
W <wang@WdeMacBook-Pro.local>
2024-05-11 21:45:31 -0700

    c

--- src/CapstoneCodeV1/Control_Options_Terminal.py
+++ src/CapstoneCodeV1/Control_Options_Terminal.py
@@ -132,7 +132,7 @@ def Terminal_Control(motor):
             print(parsed_elements)
             if(-0.1 < parsed_elements[0] < 1.1):
                 arm.setPosition(1, round(500 + 500 * parsed_elements[0]), wait=False)
-            arm.setPosition(2, round(500 + 600 (parsed_elements[1]/180)), wait=False)
+            arm.setPosition(2, round(500 + 600*(parsed_elements[1]/180)), wait=False)
         if mode == 'arm':
             L1 = 4.5
             L2 = 6.1
